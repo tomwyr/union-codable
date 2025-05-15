5e7aeeb906f276c492c243d7f1ec1a18ba34bb32
Tomasz Wyrowiński <tomwyr@hotmail.com>
2025-05-15 08:56:07 +0200

    Update product target

--- Package.swift
+++ Package.swift
@@ -9,7 +9,7 @@ let package = Package(
     products: [
         .library(
             name: "UnionCodable",
-            targets: ["UnionCodable"],
+            targets: ["UnionCodableMacros"]
         )
     ],
     dependencies: [
